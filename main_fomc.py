6778df207b16d9e2ee2d8ec3298da2b4199b25c3
j225372000 <j225372000@gmail.com>
2026-06-18 10:51:42 +0800

    Update main_fomc.py

--- main_fomc.py
+++ main_fomc.py
@@ -151,10 +151,13 @@ def main():
         f"{FOMC_INPUT_DIR}/sep.pdf"
     )
 
-    press_conference = read_input(
-        f"{FOMC_INPUT_DIR}/press_conference.pdf"
-    )
+    press_conference_path = f"{FOMC_INPUT_DIR}/press_conference.pdf"
 
+if os.path.exists(press_conference_path):
+    press_conference = read_input(press_conference_path)
+else:
+    print("未提供記者會逐字稿")
+    press_conference = ""
     dotplot_path = f"{FOMC_INPUT_DIR}/dotplot.png"
 
     print("本次聲明稿字數：", len(statement_current))
